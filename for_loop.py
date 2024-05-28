3cd6473137a83843b08f0d5fccc3b76ce31abd3e
joestonecodes <118644836+joestonecodes@users.noreply.github.com>
2024-05-28 18:18:36 +0000

    Created new file

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+#i = 0
+#numbers_divisible_by3 = 0
+#while numbers_divisible_by3 < 10: 
+#    i = i +1 
+#    if i % 3 == 0:
+#        numbers_divisible_by3 = numbers_divisible_by3 +1
+#        print(i)
+#my_name = "Joe Stone"
+
+#for i in range(len(my_name)): # i know what I need
+#    print(my_name[i])
+#    i = 1 + 1
+
+names = ["bob", "janet", "herbert", "satan"]    
+
+for name in names:
+    print(name + "@themultiverse.school")
