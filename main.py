7ac09c32a25e8f11f944b8e176402425ffacabf8
joestonecodes <118644836+joestonecodes@users.noreply.github.com>
2024-05-28 17:26:57 +0000

    passord check

---
+++ main.py
@@ -0,0 +1,15 @@
+password = input("please set a new password")
+special_character_check = "!" in password
+
+password_length_check = len(password) < 16
+
+if len(password) < 16 and len(password) > 5:
+    print("password is the right length")
+elif len(password) < 5:
+    print("the password is too short")
+else:
+    print("passwprd must be between 5 and 16 characters")
+if special_character_check:
+    print("special character found")
+else:
+    print("This password needs an ! characther somewhere")
