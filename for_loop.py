cc07baf96fb7227d834cf7ad1c4e19bd2fdf5d5a
joestonecodes <118644836+joestonecodes@users.noreply.github.com>
2024-05-28 18:23:05 +0000

    added notes

--- for_loop.py
+++ for_loop.py
@@ -1,6 +1,6 @@
 #i = 0
 #numbers_divisible_by3 = 0
-#while numbers_divisible_by3 < 10: 
+#while numbers_divisible_by3 < 10: # not sure of how many numbers I need
 #    i = i +1 
 #    if i % 3 == 0:
 #        numbers_divisible_by3 = numbers_divisible_by3 +1
@@ -11,7 +11,7 @@
 #    print(my_name[i])
 #    i = 1 + 1
 
-names = ["bob", "janet", "herbert", "satan"]    
+names = ["bob", "janet", "herbert", "satan"]    #an array
 
 for name in names:
     print(name + "@themultiverse.school")
