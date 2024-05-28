300d60ba1eb6599071926e85cfb200ed1bc3b1d2
joestonecodes <118644836+joestonecodes@users.noreply.github.com>
2024-05-28 17:44:33 +0000

    fixed formatting

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-password = input("please set a new password :")
-special_character_check = "!" in password
+valid_password = False
 
-password_length_check = len(password) < 16
+#until we get a valid password
+while not valid_password:
 
-if len(password) < 16 and len(password) > 5:
-    print("password is the right length")
-elif len(password) < 5:
-    print("the password is too short")
-else:
-    print("passwprd must be between 5 and 16 characters")
-if special_character_check:
-    print("special character found")
-else:
-    print("This password needs an ! characther somewhere")
+    password = input("please set a new password :")
+    
+    special_character_check = "!" in password
+    length_valid = False
+
+    password_length_check = len(password) < 16
+
+    if len(password) < 16 and len(password) > 5:
+        print("password is the right length")
+        length_valid = True
+    elif len(password) < 5:
+        print("the password is too short")
+    else:
+        print("passwprd must be between 5 and 16 characters")
+
+    special_characther_valid = False
+    if special_character_check:
+        print("special character found")
+        special_characther_valid = True
+    else:
+        print("This password needs an ! characther somewhere")
+    
+    if length_valid and special_characther_valid:
+        print("Your password has been set")
+        valid_password = True
+
+print ("thank you for the valid password")
